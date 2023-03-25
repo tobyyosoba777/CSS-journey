64ea87cbf2dbb9148e7dd6076af4e4fedc5a49a6
tobyyosoba777 <tobiosoba777@gmail.com>
2023-03-25 17:57:05 -0500

    updated

---
+++ 12 columns/script1.js
@@ -0,0 +1,12 @@
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+    if (darkmode.classList.contains("bx-moon")) {
+        darkmode.classList.replace("bx-moon", "bx-sun");
+        document.body.classList.add("color")
+    }
+    else {
+        darkmode.classList.replace("bx-sun", "bx-moon");
+        document.body.classList.remove("color");
+    }
+}
