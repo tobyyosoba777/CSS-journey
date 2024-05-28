de4e97fa9dbcc6c19c1046f2d97e4019eb789cbc
tobiloba <tobiosoba777@gmail.com>
2024-05-28 15:12:01 -0700

    Updated

--- Reponsive-Navbar/1/script.js
+++ Reponsive-Navbar/1/script.js
@@ -1,6 +1,19 @@
-let items = document.querySelector(".items");
-let hamburger = document.querySelector('.hamburger')
+// let items = document.querySelector(".items");
+// let hamburger = document.querySelector('.hamburger')
 
-hamburger.addEventListener('click', ()=> {
+// hamburger.addEventListener('click', ()=> {
     
+// })
+
+let bgToggle = document.querySelector('.dark-light-toggle');
+let iconToggle = document.querySelector('.dark-light-toggle i');
+
+bgToggle.addEventListener("click", () => {
+    document.body.classList.toggle('dark')
+    if(iconToggle.classList.contains('ri-moon-fill')){
+        iconToggle.classList.replace('ri-moon-fill', 'ri-sun-fill');
+    }
+    else {
+        iconToggle.classList.replace('ri-sun-fill', 'ri-moon-fill');
+    }
 })
